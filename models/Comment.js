5f8a7af9e640c81ea8399791e11bc01beb197c34
Allison House <ajhouse05@gmail.com>
2026-05-06 16:34:37 +0200

    remove server files

--- models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    animal: { type: mongoose.Schema.Types.ObjectId, ref: 'Animal', required: true },
-    author: { type: String, required: true },
-    body: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', commentSchema);
