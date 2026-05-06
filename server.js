e4399f5d9a07bd256a29c11df049107da9e8f425
Allison House <ajhouse05@gmail.com>
2026-05-06 09:59:54 +0200

    port change for deployment

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use('/api/animals', animalRoutes);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server running on port ${process.env.PORT || 5000}`);
         });
     })
     .catch(err => console.error('DB connection failed:', err));
